4f8a79bca96542f6eddf316ff6bde647d32d89b2
ajhamdi <ajhamdi@yahoo.com>
2019-04-22 18:25:36 +0300

    first message

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'NDInterval',         # How you named your package folder (MyLib)
+  packages = ['ndinterval'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'construct N-D Intervals and allow for operations on this data strucutre',   # Give a short description about your library
+  author = 'Abdullah Hamdi',                   # Type in your name
+  author_email = 'abdullah.hamdi@kaust.edu.sa',      # Type in your E-Mail
+  url = 'https://github.com/ajhamdi/NDInterval',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ajhamdi/NDInterval/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['intervals', 'n-dimensions', 'bounds'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pyinterval',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
